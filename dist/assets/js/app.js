a98be19eac8500405d298aa86243b8fbf8ea1f5f
Pavel Borisov <pavellbor@gmail.com>
2020-11-05 11:08:16 +0300

    cards sider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -5,9 +5,38 @@ $('.search__button').click(() => {
   $('.search__button').addClass('search__button--hidden');
   $('.search-form').removeClass('search-form--hidden');
 })
-$('.slider__list').slick({
-  dots: true
+$(".slider__list").slick({
+  dots: true,
 });
+
+$(".cards__list").slick({
+  arrows: false,
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  // variableWidth: false,
+  responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      // variableWidth: false,
+    }
+  },
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      // variableWidth: false,
+      dots: true,
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]});
 const cardWrapper = $('.cards-item__images');
 cardWrapper.mousemove(function(e) {
   let pos = $(this).offset();
--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -1,3 +1,32 @@
-$('.slider__list').slick({
-  dots: true
+$(".slider__list").slick({
+  dots: true,
 });
+
+$(".cards__list").slick({
+  arrows: false,
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  // variableWidth: false,
+  responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      // variableWidth: false,
+    }
+  },
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      // variableWidth: false,
+      dots: true,
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]});
